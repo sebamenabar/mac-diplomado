fd90af2f8c698bddaa8d594447156271f704402d
Sebastian Amenabar <samenabar@uc.cl>
2019-09-03 17:42:39 -0300

    add epochs as inline arguments

--- code/config.py
+++ code/config.py
@@ -19,6 +19,7 @@ __C.TEST_BATCH_SIZE = 256
 __C.SAMPLE = False
 __C.resume_model = None
 __C.resume_model_ema = None
+__C.start_epoch = None
 # Training options
 __C.TRAIN = edict()
 __C.TRAIN.FLAG = True
